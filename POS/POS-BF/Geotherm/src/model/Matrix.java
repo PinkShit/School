95d1a5b9bd156b9a724616d5773adab8ce7d4a19
PinkLabs <33427704+PinkShit@users.noreply.github.com>
2018-05-28 22:18:58 +0200

    Update Matrix.java

--- POS/POS-BF/Geotherm/src/model/Matrix.java
+++ POS/POS-BF/Geotherm/src/model/Matrix.java
@@ -2,13 +2,21 @@ package model;
 
 import java.util.*;
 
+import view.GPanel;
+
 public class Matrix {
 	
-	private int[][] matrix;
+	public int[][] matrix;
 	private ArrayList<ArrayList<Integer>> matrix2;
 	private Random rand;
+	GPanel gpan;
+	
+	private int left;
+	private int right;
 	
 	public Matrix(int left, int right) {
+		this.left = left;
+		this.right = right;
 		matrix = new int[left][right];
 	}
 	
@@ -42,15 +50,41 @@ public class Matrix {
 	public void printMatrix() {
 		for(int i = 0 ; i<matrix.length;i++) {
 			System.out.println();
-			
 			for(int j = 0 ; j<matrix.length;j++) {
 				
 				int zahl = (int)((Math.random()) * 9);
 				
 				matrix[i][j] = zahl;
+				
+				//damit wird die matrix symmetrischss
+				matrix[i][j] = matrix[j][i];
 
 				
 				System.out.print("| "+matrix[i][j]+" |");
+				
+			}
+		}
+	}
+	
+	public String[][] addTextArea() {
+		String a = "";
+		String[][] arr = new String[left][right];
+		
+		for(int i = 0 ; i<matrix.length;i++) {
+			System.out.println();
+			for(int j = 0 ; j<matrix.length;j++) {
+				
+				arr[i][j] = ""+ matrix[i][j];
+				System.out.print(arr[i][j]);
+			}
+		}
+		return arr;
+				
+	}
+	
+	public void multiplyMatrix(Matrix n, Matrix m) {
+		for(int i = 0 ; i<matrix.length;i++) {
+			for(int j = 0 ; j<matrix.length;j++) {
 				
 				
 			}
