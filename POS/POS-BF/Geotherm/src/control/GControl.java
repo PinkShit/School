a4804602da19913c35aa59a12c02c3b0e98a6269
PinkLabs <33427704+PinkShit@users.noreply.github.com>
2018-05-28 22:18:27 +0200

    Update GControl.java

--- POS/POS-BF/Geotherm/src/control/GControl.java
+++ POS/POS-BF/Geotherm/src/control/GControl.java
@@ -10,25 +10,45 @@ public class GControl implements ActionListener{
 	
 	//Attribute
 	private GFunktionen gfun;
-	private GPanel gpan;
+	private static GPanel gpan;
 	private Matrix ma;
 	
-	public GControl(){
+	/*
+	 * 
+	 */
+	public GControl(GPanel gpan){
 		gfun = new GFunktionen();
-		gpan = new GPanel();
-		new GFrame("GeoTherm",gpan);
-		ma = new Matrix(15,15);
-		ma.fuellenZufaellig();
-		ma.printMatrix();
+		this.gpan = gpan;
 	}
 	
+	/*
+	 * 
+	 */
 	public static void main(String[] args) {
-		new GControl();
+		new GControl(new GPanel());
+		new GFrame("GeoTherm",gpan);
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent a) {
-		
-		
-	}
+	/*
+	 * 
+	 */
+    @Override
+    public void actionPerformed(ActionEvent e) {        	
+        if (e.getSource() ==  gpan.getRandom()) {
+            ma = new Matrix(15,15);
+        	ma.fuellenZufaellig();
+        	
+        	String a = "";
+        	for(int i = 0 ; i<ma.matrix.length;i++) {
+    			for(int j = 0 ; j<ma.matrix.length;j++) {
+    				
+    				a = a + ma.matrix[i][j];
+    				
+    				gpan.jtf.setText(gpan.jtf.getText()+""+a);
+    				a ="";
+    			}
+    		}
+        	
+        }
+    }
 }
