0f180221f20376e09717409c79f5e29be454c185
PinkLabs <33427704+PinkShit@users.noreply.github.com>
2018-05-28 22:19:58 +0200

    Update GFrame.java

--- POS/POS-BF/Geotherm/src/view/GFrame.java
+++ POS/POS-BF/Geotherm/src/view/GFrame.java
@@ -8,7 +8,7 @@ public class GFrame extends JFrame{
 	public GFrame(String title, GPanel p) {
 		setTitle(title);
 		setVisible(true);
-		setSize(1920,1080);
+		setSize(1000,1000);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		add(p);
 		
